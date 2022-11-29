3ba0f27a882b910cb191a9c26980a2376af8139d
Zeynel Şahin <50925966+zeynelsahin@users.noreply.github.com>
2022-11-29 23:19:11 +0300

    Asserting numeric

--- EmployeeManagement.Test/EmployeeFactoryTests.cs
+++ EmployeeManagement.Test/EmployeeFactoryTests.cs
@@ -11,6 +11,31 @@ public class EmployeeFactoryTests
     var employeeFactory = new EmployeeFactory();
     var employee = (InternalEmployee)employeeFactory.CreateEmployee("Zeynel", "Sahin");
     
-    Assert.True(employee.Salary is >= 2500 and <= 3500);
+    Assert.True(employee.Salary is >= 2500 and <= 3500,"employee.Salary is >= 2500 and <= 3500");
   }
+  [Fact]
+  public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500_Alternative()
+  {
+    var employeeFactory = new EmployeeFactory();
+    var employee = (InternalEmployee)employeeFactory.CreateEmployee("Zeynel", "Sahin");
+
+    Assert.True(employee.Salary is >= 2500);
+    Assert.True(employee.Salary <= 3500);
+  }
+  [Fact]
+  public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500_AlternativeWithInRange()
+  {
+    var employeeFactory = new EmployeeFactory();
+    var employee = (InternalEmployee)employeeFactory.CreateEmployee("Zeynel", "Sahin");
+
+    Assert.InRange(employee.Salary, 2000, 3400);
+  } 
+  [Fact]
+  public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500_PrecisionExample()
+  {
+    var employeeFactory = new EmployeeFactory();
+    var employee = (InternalEmployee)employeeFactory.CreateEmployee("Zeynel", "Sahin");
+    employee.Salary = 2500.123m;
+    Assert.Equal(2500,employee.Salary,0);//precision virgülden sonra kaç basamağın dikkate alıncağını belirtir default değeri tümü dür
+  } 
 } 
