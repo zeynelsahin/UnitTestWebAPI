87f89d3380f9acf63e039750bff3f855d4648496
Zeynel Şahin <50925966+zeynelsahin@users.noreply.github.com>
2022-12-01 19:31:58 +0300

    Dependency Injection

---
+++ EmployeeManagement.Test/EmployeeServiceTestWithAspNetCoreDI.cs
@@ -0,0 +1,23 @@
+﻿using EmployeeManagement.Test.Fixtures;
+
+namespace EmployeeManagement.Test;
+
+public class EmployeeServiceTestWithAspNetCoreDI: IClassFixture<EmployeeServiceWithAspNetCoreDIFixture>
+{
+    private readonly EmployeeServiceWithAspNetCoreDIFixture _employeeServiceWithAspNetCoreDiFixture;
+
+    public EmployeeServiceTestWithAspNetCoreDI(EmployeeServiceWithAspNetCoreDIFixture employeeServiceWithAspNetCoreDiFixture)
+    {
+        _employeeServiceWithAspNetCoreDiFixture = employeeServiceWithAspNetCoreDiFixture;
+    }
+
+    [Fact]
+    public void CreateInternalEmployee_InternalEmployeeCreated_MustHaveAttendedFirstObligatory()
+    {
+        var obligatoryCourse= _employeeServiceWithAspNetCoreDiFixture.EmployeeManagementRepository.GetCourse(Guid.Parse("37e03ca7-c730-4351-834c-b66f280cdb01"));
+        
+        var internalEmployee = _employeeServiceWithAspNetCoreDiFixture.EmployeeService.CreateInternalEmployee("Zeynel", "Sahin");
+
+        Assert.Contains(obligatoryCourse, internalEmployee.AttendedCourses);
+    }
+}
---
+++ EmployeeManagement.Test/Fixtures/EmployeeServiceWithAspNetCoreDIFixture.cs
@@ -0,0 +1,26 @@
+﻿using EmployeeManagement.Business;
+using EmployeeManagement.DataAccess.Services;
+using EmployeeManagement.Services.Test;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmployeeManagement.Test.Fixtures;
+
+public class EmployeeServiceWithAspNetCoreDIFixture: IDisposable
+{
+    private readonly ServiceProvider _serviceProvider;
+    public IEmployeeManagementRepository EmployeeManagementRepository => _serviceProvider.GetService<IEmployeeManagementRepository>() ?? throw new InvalidOperationException();
+
+    public IEmployeeService? EmployeeService => _serviceProvider.GetService<IEmployeeService>();
+
+    public EmployeeServiceWithAspNetCoreDIFixture()
+    {
+        var services = new ServiceCollection();
+        services.AddScoped<EmployeeFactory>();
+        services.AddScoped<IEmployeeManagementRepository, EmployeeManagementTestDataRepository>();
+        services.AddScoped<IEmployeeService, EmployeeService>();
+        _serviceProvider = services.BuildServiceProvider();
+    }
+    public void Dispose()
+    {
+    }
+}
