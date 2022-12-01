0c5c6f86830fbef26f42e5c65a4c34f01bc26cc6
Zeynel Şahin <50925966+zeynelsahin@users.noreply.github.com>
2022-12-01 22:12:10 +0300

    Theory with Member Data

--- EmployeeManagement.Test/DataDrivenEmployeeServiceTests.cs
+++ EmployeeManagement.Test/DataDrivenEmployeeServiceTests.cs
@@ -14,7 +14,7 @@ public class DataDrivenEmployeeServiceTests : IClassFixture<EmployeeServiceFixtu
     {
         _employeeServiceFixture = employeeServiceFixture;
     }
-    
+
     [Theory]
     [InlineData("1fd115cf-f44c-4982-86bc-a8fe2e4ff83e")]
     [InlineData("37e03ca7-c730-4351-834c-b66f280cdb01")]
@@ -23,11 +23,57 @@ public class DataDrivenEmployeeServiceTests : IClassFixture<EmployeeServiceFixtu
         var internalEmployee = _employeeServiceFixture.EmployeeService.CreateInternalEmployee("Zeynel", "Sahin");
         Assert.Contains(internalEmployee.AttendedCourses, course => course.Id == courseId);
     }
+
     [Fact]
-    public async Task GiveRaise_MoreThanMinimumRaiseGiven_EmployeeMinimumRaiseGivenMustBe()
+    public async Task GiveRaise_MinimumRaiseGiven_EmployeeMinimumRaiseGivenMustBeTrue()
     {
-        var internalEmployee = new InternalEmployee("Zeynel", "Sahin", 4, 3500, false, 1);
-        await _employeeServiceFixture.EmployeeService.GiveRaiseAsync(internalEmployee, 200);
+        var internalEmployee = new InternalEmployee(
+            "Zeynel", "Sahin", 5, 3000, false, 1);
+        await _employeeServiceFixture
+            .EmployeeService.GiveRaiseAsync(internalEmployee, 100);
+        Assert.True(internalEmployee.MinimumRaiseGiven);
+    }
+
+
+    [Fact]
+    public async Task GiveRaise_MoreThanMinimumRaiseGiven_EmployeeMinimumRaiseGivenMustBeFalse()
+    {
+        var internalEmployee = new InternalEmployee(
+            "Zeynel", "Sahin", 5, 3000, false, 1);
+        await _employeeServiceFixture.EmployeeService
+            .GiveRaiseAsync(internalEmployee, 200);
         Assert.False(internalEmployee.MinimumRaiseGiven);
+    }
+
+    public static IEnumerable<object[]> ExampleTestDataForGiveRaiseWithProperty
+    {
+        get
+        {
+            return new List<object[]>
+            {
+                new object[] { 100, true },
+                new object[] { 200, false },
+            };
+        }
+    }
+
+    public static IEnumerable<object[]> ExampleTestDataForGiveRaiseWithMethod(int testDataInstancesToProvide)
+    {
+        var testData=new List<object[]>
+        {
+            new object[] { 100, true },
+            new object[] { 200, false },
+        };
+        return testData.Take(testDataInstancesToProvide);
+    }
+
+    [Theory]
+    [MemberData(nameof(ExampleTestDataForGiveRaiseWithMethod),1,MemberType = typeof(DataDrivenEmployeeServiceTests)),]
+    public async Task GiveRaise_RaiseGive_EmployeeMinimumRaiseGivenMatchesValue(int raiseGiven, bool exceptedValueForMinimumRaiseGiven)
+    {
+        var internalEmployee = new InternalEmployee(
+            "Zeynel", "Sahin", 5, 3000, false, 1);
+        await _employeeServiceFixture.EmployeeService.GiveRaiseAsync(internalEmployee, raiseGiven);
+        Assert.Equal(exceptedValueForMinimumRaiseGiven, internalEmployee.MinimumRaiseGiven);
     }
 }
