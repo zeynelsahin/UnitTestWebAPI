1f5ce0970094bc8870425ca2f38475ef0cfe1511
Zeynel Şahin <50925966+zeynelsahin@users.noreply.github.com>
2022-12-03 00:59:16 +0300

    Moc async and interface

--- EmployeeManagement.Test/MoqTests.cs
+++ EmployeeManagement.Test/MoqTests.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper.Configuration.Annotations;
 using EmployeeManagement.Business;
 using EmployeeManagement.DataAccess.Entities;
+using EmployeeManagement.DataAccess.Services;
 using EmployeeManagement.Services.Test;
 using Moq;
 
@@ -25,16 +26,54 @@ public class MoqTests
         var employeeManagementTestDataRepository = new EmployeeManagementTestDataRepository();
         var employeeFactoryMock = new Mock<EmployeeFactory>();
         employeeFactoryMock.Setup(p => p.CreateEmployee("Zeynel", It.IsAny<string>(), null, false))
-            .Returns(new InternalEmployee("Zeynel","Sahin",5,2700,false,1));
-        employeeFactoryMock.Setup(p => p.CreateEmployee(It.Is<string>(value=>value.Contains('z')), It.IsAny<string>(), null, false))
-            .Returns(new InternalEmployee("Zeynel","Sahin",5,2700,false,1));
+            .Returns(new InternalEmployee("Zeynel", "Sahin", 5, 2700, false, 1));
+        employeeFactoryMock.Setup(p => p.CreateEmployee(It.Is<string>(value => value.Contains('z')), It.IsAny<string>(), null, false))
+            .Returns(new InternalEmployee("Zeynel", "Sahin", 5, 2700, false, 1));
         employeeFactoryMock.Setup(p => p.CreateEmployee("Yavuz", It.IsAny<string>(), null, false))
-            .Returns(new InternalEmployee("Zeynel","Sahin",5,2700,false,1));
+            .Returns(new InternalEmployee("Zeynel", "Sahin", 5, 2700, false, 1));
 
         var employeeService = new EmployeeService(employeeManagementTestDataRepository, employeeFactoryMock.Object);
 
         const decimal suggestedBonus = 1000;
         var employee = employeeService.CreateInternalEmployee("Zeynel", "Sahin");
         Assert.Equal(suggestedBonus, employee.SuggestedBonus);
+    }
+
+    [Fact]
+    public void FetchInternalEmployee_EmployeeFetched_SuggestedBonusMustBeCalculated_MoqInterface()
+    {
+        var employeeManagementTestDataRepositoryMock = new Mock<IEmployeeManagementRepository>();
+
+        employeeManagementTestDataRepositoryMock.Setup(p => p.GetInternalEmployee(It.IsAny<Guid>()))
+            .Returns(new InternalEmployee("Zeynel", "Sahin", 2, 2500, false, 2)
+            {
+                AttendedCourses = new List<Course>()
+                {
+                    new Course("A course"), new Course("Another course")
+                }
+            });
+
+        var employeeFactoryMock = new Mock<EmployeeFactory>();
+        var employeeService = new EmployeeService(employeeManagementTestDataRepositoryMock.Object, employeeFactoryMock.Object);
+        var employee = employeeService.FetchInternalEmployee(Guid.Empty);
+        Assert.Equal(400, employee.SuggestedBonus);
+    }
+    public async Task FetchInternalEmployee_EmployeeFetched_SuggestedBonusMustBeCalculated_MoqInterface_Async()
+    {
+        var employeeManagementTestDataRepositoryMock = new Mock<IEmployeeManagementRepository>();
+
+        employeeManagementTestDataRepositoryMock.Setup(p => p.GetInternalEmployeeAsync(It.IsAny<Guid>()))
+            .ReturnsAsync(new InternalEmployee("Zeynel", "Sahin", 2, 2500, false, 2)
+            {
+                AttendedCourses = new List<Course>()
+                {
+                    new Course("A course"), new Course("Another course")
+                }
+            });
+
+        var employeeFactoryMock = new Mock<EmployeeFactory>();
+        var employeeService = new EmployeeService(employeeManagementTestDataRepositoryMock.Object, employeeFactoryMock.Object);
+        var employee = await employeeService.FetchInternalEmployeeAsync(Guid.Empty);
+        Assert.Equal(400, employee.SuggestedBonus);
     }
 }
