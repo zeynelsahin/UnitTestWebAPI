41007ea7e093c3f1c8204897b529001be0cdcb89
Zeynel Şahin <50925966+zeynelsahin@users.noreply.github.com>
2022-12-03 19:26:59 +0300

    AutoMapper dependencies

--- EmployeeManagement.Test/InternalEmployeeControllerTests.cs
+++ EmployeeManagement.Test/InternalEmployeeControllerTests.cs
@@ -1,33 +1,42 @@
-﻿using EmployeeManagement.Business;
+﻿using AutoMapper;
+using EmployeeManagement.Business;
 using EmployeeManagement.Controllers;
 using EmployeeManagement.DataAccess.Entities;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Moq;
+using Xunit.Abstractions;
 
 namespace EmployeeManagement.Test;
 
 public class InternalEmployeeControllerTests
 {
     private readonly InternalEmployeesController _internalEmployeesController;
+    private readonly InternalEmployee _firstEmployee;
+    private readonly ITestOutputHelper _testOutputHelper;
 
-    public InternalEmployeeControllerTests()
+    public InternalEmployeeControllerTests(ITestOutputHelper testOutputHelper)
     {
+        _testOutputHelper = testOutputHelper;
+
+        _firstEmployee = new InternalEmployee("Zeynel", "Sahin", 2, 3000, false, 2);
         var employeeServiceMock = new Mock<IEmployeeService>();
         employeeServiceMock.Setup(p => p.FetchInternalEmployeesAsync()).ReturnsAsync(new List<InternalEmployee>()
         {
-            new InternalEmployee("Zeynel", "Sahin", 2, 3000, false, 2),
+            _firstEmployee,
             new InternalEmployee("Zeynel", "Sahin", 3, 3400, false, 1),
             new InternalEmployee("Zeynel", "Sahin", 3, 4000, false, 3)
         });
+        // var mapperMock = new Mock<IMapper>();
+        // mapperMock.Setup(m => m.Map<InternalEmployee, Models.InternalEmployeeDto>(It.IsAny<InternalEmployee>())).Returns(new Models.InternalEmployeeDto());
 
-        _internalEmployeesController = new InternalEmployeesController(employeeServiceMock.Object, null);
-
+        var mapperConfiguration = new MapperConfiguration(configure=>configure.AddProfile<MapperProfiles.EmployeeProfile>());
+        var mapper = new Mapper(mapperConfiguration);
+        _internalEmployeesController = new InternalEmployeesController(employeeServiceMock.Object, mapperMock);
     }
+
     [Fact]
     public async Task GetInternalEmployees_GetAction_MustReturnOkObjectResult()
     {
-     
         var result = await _internalEmployeesController.GetInternalEmployees();
         var actionResult = Assert.IsType<ActionResult<IEnumerable<Models.InternalEmployeeDto>>>(result);
 
@@ -50,7 +59,20 @@ public class InternalEmployeeControllerTests
 
         var actionResult = Assert.IsType<ActionResult<IEnumerable<Models.InternalEmployeeDto>>>(resul);
 
-        Assert.Equal(3,((IEnumerable<Models.InternalEmployeeDto>)
+        Assert.Equal(3, ((IEnumerable<Models.InternalEmployeeDto>)
             ((OkObjectResult)actionResult.Result).Value).Count());
+    }
+
+    [Fact]
+    public async Task GetInternalEmployees_GetAction_ReturnsOkObjectResultWithCorrectAmountOfInternalEmployees()
+    {
+        var result = await _internalEmployeesController.GetInternalEmployees();
+
+        var actionResult = Assert.IsType<ActionResult<IEnumerable<Models.InternalEmployeeDto>>>(result);
+        var okObjectResult = Assert.IsType<OkObjectResult>(actionResult.Result);
+        var dtoList = Assert.IsAssignableFrom<IEnumerable<Models.InternalEmployeeDto>>(okObjectResult.Value);
+
+        Assert.Equal(3, dtoList.Count());
+        _testOutputHelper.WriteLine($"Dto List Count = {dtoList.Count()}");
     }
 }
