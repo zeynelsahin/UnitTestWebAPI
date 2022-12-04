1a1c41ec013769393c33ef3bb81b71ce44b34911
Zeynel Şahin <50925966+zeynelsahin@users.noreply.github.com>
2022-12-04 15:09:52 +0300

    Service Registrations

---
+++ EmployeeManagement.Test/ServiceCollectionTests.cs
@@ -0,0 +1,24 @@
+﻿using EmployeeManagement.Business;
+using EmployeeManagement.DataAccess.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmployeeManagement.Test;
+
+public class ServiceCollectionTests
+{
+    [Fact]
+    public void RegisterDataServices_Execute_DataServicesAreRegistered()
+    {
+        var serviceCollection = new ServiceCollection();
+        var configuration = new ConfigurationBuilder().AddInMemoryCollection(new Dictionary<string, string>()
+        {
+            { "ConnectionStrings:EmployeeManagementDB", "AnyValueWillDo" }
+        }).Build();
+        serviceCollection.RegisterDataServices(configuration);
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+
+        Assert.NotNull(serviceProvider.GetService<IEmployeeManagementRepository>());
+        Assert.IsType<EmployeeManagementRepository>(serviceProvider.GetService<IEmployeeManagementRepository>());
+    }
+}
